a9e9aa858a42d38051f98eed20fa3bd6c7c92214
brotke <nikbetke@gmail.com>
2021-10-20 12:22:00 +0200

    exercise 1.1

--- part1/courseinformation/src/App.js
+++ part1/courseinformation/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
 
 const App = () => {
+  
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -8,20 +12,15 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header heading={course}/>
+      <Content part = {part1} exercise = {exercises1}/>
+      <Content part = {part2} exercise = {exercises2}/>
+      <Content part = {part3} exercise = {exercises3}/>
+      <Total total = {total}/>
     </div>
   )
 }
